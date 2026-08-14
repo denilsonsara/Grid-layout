48c4ab535c97f09821f32d0d23168279ad418808
denisonsara <denilson.sara@gmail.com>
2026-08-14 20:18:26 -0300

    criacao do script.js , usuario.js

---
+++ Atividade WhatsApp Web/script.js
@@ -0,0 +1,42 @@
+import { listarUsuarios, listarContatosUsuario } from "./assets/usuario.js";
+
+listarContatosUsuario(2);
+
+const elemento = {
+  form_send_message: document.querySelector("#form-send-msg"),
+  input_send_message: document.querySelector("#input-send-msg"),
+  grid_container: document.querySelector(".grid-msg"),
+};
+
+elemento.form_send_message.addEventListener("submit", (e) => {
+  e.preventDefault();
+  insertMessage(elemento.input_send_message.value);
+});
+
+function getHour() {
+  const data = new Date().toLocaleString("pt-BR", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+
+  return data;
+}
+
+function insertMessage(message) {
+  const article = document.createElement("article");
+  const paragrafo = document.createElement("p");
+  const span = document.createElement("span");
+
+  article.classList.add("enviadas");
+
+  paragrafo.innerText = message;
+  span.classList.add("hour");
+  span.innerText = `${getHour()}`;
+
+  article.append(paragrafo, span);
+
+  elemento.grid_container.append(article);
+  elemento.grid_container.scrollTop = elemento.grid_container.scrollHeight;
+  elemento.input_send_message.value = "";
+}
